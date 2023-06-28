97121fb8e90230e8f316684f4a04cd36d3a19117
username <1437061099@qq.com>
2023-06-28 10:23:33 +0800

    再次提交

--- App.vue
+++ App.vue
@@ -4,7 +4,7 @@
 			// console.warn('当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！')
 			// console.log('App Launch1')
 			console.log('测试合并主分支')
-			// console.log('App Show')
+			console.log('App Show')
 		},
 		onShow: function() {
 			// console.log('App Show')

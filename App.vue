ee135ffd5e77b0dc526c6d67c73192f364997794
赵文浩 <1437061099@qq.com>
2023-08-21 21:14:24 +0800

    fix: 删除无用的log日志

--- App.vue
+++ App.vue
@@ -1,30 +1,28 @@
 <script>
-	export default {
-		onLaunch: function() {
-         console.log(1);
-      },
-		onShow: function() {},
-		onHide: function() {}
-	}
+export default {
+  onLaunch: function () {},
+  onShow: function () {},
+  onHide: function () {},
+};
 </script>
 
 <style lang="scss">
-	/*每个页面公共css */
-	@import '@/uni_modules/uni-scss/index.scss';
-	@import '/wxcomponents/vant/common/index.wxss';
-	/* #ifndef APP-NVUE */
-	@import '@/static/customicons.css';
+/*每个页面公共css */
+@import "@/uni_modules/uni-scss/index.scss";
+@import "/wxcomponents/vant/common/index.wxss";
+/* #ifndef APP-NVUE */
+@import "@/static/customicons.css";
 
-	page {
-		background-color: #f5f5f5;
-	}
+page {
+  background-color: #f5f5f5;
+}
 
-	/* #endif */
-	.example-info {
-		font-size: 14px;
-		color: #333;
-		padding: 10px;
-	}
+/* #endif */
+.example-info {
+  font-size: 14px;
+  color: #333;
+  padding: 10px;
+}
 </style>
 <style>
 /* 	@import 'vant/lib/action-sheet/index.css';
